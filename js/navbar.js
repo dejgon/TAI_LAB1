c0a6500d087c2114992bb71dd3082ff64aa5e437
dejgon <cop.damian@gmail.com>
2019-03-15 15:39:27 +0100

    Add scrolling to top

--- js/navbar.js
+++ js/navbar.js
@@ -18,4 +18,21 @@ let nav = '<nav class="navbar navbar-light bg-light">' +
 
 let elem = document.querySelector('.navbar');
 elem.innerHTML = nav;
-console.log("Done")
+console.log("Done");
+let scrollBtn = document.querySelector('.scrollBtn');
+let scroll = document.querySelector('.scroll');
+
+scroll.addEventListener('click', topFunction);
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scroll.style.display = "block";
+    } else {
+      scroll.style.display = "none";
+    }
+  }
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
